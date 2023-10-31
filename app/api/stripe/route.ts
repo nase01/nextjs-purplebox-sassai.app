8d6075a3c38f24afade2cece4042f5ebc69e6cf5
nase01 <naessony01@gmail.com>
2023-10-31 21:07:25 +0800

    Change name

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -41,7 +41,7 @@ export async function GET() {
           price_data: {
             currency: "USD",
             product_data: {
-              name: "AIMatrix Pro",
+              name: "Purple Box Pro",
               description: "Unlimited AI Generations"
             },
             unit_amount: 2000,
